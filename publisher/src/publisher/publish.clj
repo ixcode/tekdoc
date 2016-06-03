c1675aa37d1b923c8370d628fe6ba7664854db85
Jim Barritt <jim@planet-ix.com>
2016-06-03 14:36:46 +0100

    Adding multimethods to process different file types

--- publisher/src/publisher/publish.clj
+++ publisher/src/publisher/publish.clj
@@ -1,7 +1,6 @@
 (ns publisher.publish
   (:use [markdown.core]
-        [clojure.java.io]
-        [publisher.render])
+        [clojure.java.io])
   (:require [compojure.handler :as handler]
             [ring.middleware.json :as middleware]
             [compojure.route :as route]
@@ -39,13 +38,32 @@
   (let [filepath (.getPath file)
         file-details (parse-filepath filepath)]
     {:filepath filepath
+     :name (:name file-details)
      :filename (:filename file-details)
      :page-id (:filepath-without-extension file-details)
      :type (keyword (:extension file-details))}))
 
-(def list-of-page-files (map #(page-file %) (walk config/content-root content-files-pattern)))
+(defn list-page-files [root-dir]
+  (map #(page-file %) (walk root-dir content-files-pattern)))
+
+(defmulti process-page (fn [export-root page-file] (:type page-file)))
+
+(defmethod process-page :md [export-root page-file]
+  (println (str "Writing markdown file " page-file " to  " export-root)))
+
+(defmethod process-page :html [export-root page-file]
+  (println (str "Writing a selmer template " page-file " to " export-root)))
+
+(defmethod process-page :default [export-root page-file]
+  (throw (IllegalArgumentException. (str "Could not process a file of type " (:type page-file)))))
+
+;;(def test-page {:type :html})
+
+;;(let [output-file (str export-root (:page-id page-file) ".html")])
+(defn process-page-files [export-root page-files]
+  (dorun (map (partial process-page export-root) page-files)))
 
 
-(defn export-site [root-dir, export-dir]
-  )
+(defn export-site [export-root content-root]
+  (process-page-files export-root (list-page-files config/content-root)))
 
