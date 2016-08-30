0c069cce8a1bfda53efd28a80fe511262f1135bb
Jim Barritt <jim@planet-ix.com>
2016-08-30 15:21:36 +0100

    Removing relativeness of publish root

--- publisher/src/publisher/config.clj
+++ publisher/src/publisher/config.clj
@@ -26,7 +26,7 @@
 (def static-root (path-from-config :static))
 (def output-root (path-from-config :output))
 (def layout-root (path-from-config :layouts))
-(def publish-root (path-from-config :publish))
+(def publish-root (:publish site-config))
 
 
 (defn initialise! [site-config-file]  
